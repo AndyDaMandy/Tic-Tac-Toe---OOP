8984f0196f2f379cd5db7b926c10019f7e36a369
Andy M <maldonado.andy.m@gmail.com>
2022-05-08 14:32:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple Ruby Tic Tac Toe project to practice OOP
---
+++ main.rb
@@ -0,0 +1,6 @@
+class Board
+  attr_accessor :row_one, :row_two, :row_three
+  row_one = ["1", "|", "2", "|", "3"]
+  row_two = ["4", "|", "5", "|", "6"]
+  row_three = ["7", "|", "8", "|", "9"]
+end
