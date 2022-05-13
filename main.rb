88ef16d0f10bcd929f5ecd5382fb0029dcdf97c8
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-05-13 14:10:37 -0400

    got it mostly working and resolving issues

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ module GameText
       puts "It's heads! You get to go first!"
     end
     def computer_first
-      puts "It's tails! The computer gets to go first!"
+      puts "It's tails! The AI gets to go first!"
     end
     def player_select
       puts "Select a cell to place your mark!"
@@ -18,9 +18,13 @@ module GameText
     def ai_select
       puts "The AI will now select a cell...."
     end
+    def cannot_be
+      puts "Error, you must select a number from the cells available. Please select again."
+    end
 end
 
 class Game
+  attr_accessor :player, :player_score, :ai_score, :player_win, :ai_win
   include GameText
   @player = true
   @player_score = []
@@ -44,21 +48,40 @@ class Game
       @player = true
     end
   end
-  def turn
+  def turn(arr)
     if @player == true
       player_select
       input = gets
-      
+      if input arr.include? input == false
+        cannot_be
+        player_select
+        input = gets
+      end
+        arr[input] = "X"
+        @player_score.push(input)
+        puts "you selected #{input}"
+        Board.view_board
+    else
+      ai_select
+      ai_choice = rand(9)
+      while arr.include? ai_choice == false
+        ai_choice = rand(9)
+      end
+      puts "AI selected #{ai_choice}"
+      @ai_score.push(input)
+      arr[input] = "O"
+      Board.view_board
     end
-  end
+end
   def game_loop
+
   end
 end
 
 #holds the cells, winning combos
 class Board
+  attr_accessor :cells
       WIN_COMBO = [[1, 5, 9], [1, 2, 3], [2, 5, 8], [1, 4, 7], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]]  
-  attr_accessor :WIN_COMBO, :initialize, :cells
    def initialize
     @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end 
@@ -69,11 +92,11 @@ end
 
 #runs the game
 def play
-  game = Game.new
   board = Board.new
+  game = Game.new
   game.start
   game.turn_order
   board.view_board
-  game.turn
+  game.turn(board.cells)
 end
 play
